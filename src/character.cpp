7614017e041736961b6b59a1bd00c78a002635d0
Kieran Hancock <kieran@k1h.eu>
2013-07-26 22:41:46 +0100

    Added a GNU GPL v3 License and COPYING notice

--- src/character.cpp
+++ src/character.cpp
@@ -1,3 +1,21 @@
+/*
+ *  This file is part of Banana-ade.
+ *  Copyright (C) 2013 Kieran Hancock & Alex Kerr
+ *
+ *  Banana-ade is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Banana-ade is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Banana-ade.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "character.h"
 
 //Returns the sprite (check the header, is this overkill?)
